87d92c93b9909657963ff77d14fcec088d2a6be8
maono <maono@student.42tokyo.jp>
2026-04-16 17:55:44 +0900

    fix: addition

--- NngSolutions/MyNat/Addition.lean
+++ NngSolutions/MyNat/Addition.lean
@@ -5,25 +5,24 @@ namespace MyNat
 -- Level 1
 theorem zero_add (a : MyNat) : 0 + a = a := by
   induction a with
-  | zero => rw [add_zero]
+  | zero => rfl
   | succ d hd => rw [add_succ, hd]
 
 -- Level 2
 theorem succ_add (a b : MyNat) : .succ a + b = .succ (a + b) := by
   induction b with
-  | zero => rw [add_zero, add_zero]
+  | zero => rfl
   | succ d hd => rw [add_succ, add_succ, hd]
 
 -- Level 3
 theorem add_comm (a b : MyNat) : a + b = b + a := by
   induction b with
-  | zero => rw [add_zero, zero_add]
+  | zero => show a + 0 = 0 + a; rw [add_zero, zero_add]
   | succ d hd => rw [add_succ, succ_add, hd]
-
 -- Level 4
 theorem add_assoc (a b c : MyNat) : a + b + c = a + (b + c) := by
   induction a with
-  | zero => rw [zero_add, zero_add]
+  | zero => show 0 + b + c = 0 + (b + c); rw [zero_add, zero_add]
   | succ d hd => rw [succ_add, succ_add, succ_add, hd]
 
 -- Level 5
