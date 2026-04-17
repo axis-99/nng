e814764d816c4a0ea1f3e0dc4577228194771c86
maono <maono@student.42tokyo.jp>
2026-04-18 07:19:06 +0900

    feat: implication level6

--- NngSolutions/MyNat/Implication.lean
+++ NngSolutions/MyNat/Implication.lean
@@ -16,4 +16,6 @@ example (x : MyNat) (h : x + 1 = 4) : x = 3 := by
   rw [← succ_eq_add_one, four_eq_succ_three] at h
   injection h
 
-  
+example (x : MyNat) : x = 37 -> x = 37 := by
+  intro h
+  exact h
